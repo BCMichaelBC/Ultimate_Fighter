857d480c03b209c0134de6b73a127a3639af8ccb
Michael Otasowie <michaelotasowie@gmail.com>
2023-04-03 11:32:51 -0400

    The AI is finished and can do basic things like punch and kick

--- Assets/Characters/AI/CombatAI.cs
+++ Assets/Characters/AI/CombatAI.cs
@@ -13,6 +13,8 @@ public class CombatAI : MonoBehaviour
     StateManager states;
     [SerializeField] PlayerMovementAI attSTATE;
     [SerializeField] int numberAttack = 0;
+    public bool attacking = true;
+    public float atackRate = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,64 +27,68 @@ public class CombatAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKey(KeyCode.H) && states.isPlayerOne) || (Input.GetKey(KeyCode.L) && !states.isPlayerOne)) // To block
+        if(attacking == true)
         {
-            animator.SetBool("Blocking", true);
-        }
-        else
-        {
-            animator.SetBool("Blocking", false);
-        }
+            attacking = false;
+            if ((Input.GetKey(KeyCode.H) && states.isPlayerOne) || (Input.GetKey(KeyCode.L) && !states.isPlayerOne)) // To block
+            {
+                animator.SetBool("Blocking", true);
+            }
+            else
+            {
+                animator.SetBool("Blocking", false);
+            }
 
-        if ((Input.GetKey(KeyCode.S) && states.isPlayerOne) || (Input.GetKey(KeyCode.DownArrow) && !states.isPlayerOne)) // To crouch
-        {
-            animator.SetBool("Crouching", true);
-            movementcolliders[0].SetActive(false);
-            if ((Input.GetKey(KeyCode.F) && states.isPlayerOne) || (Input.GetKey(KeyCode.J) && !states.isPlayerOne)) // crouch punch
+            if ((Input.GetKey(KeyCode.S) && states.isPlayerOne) || (Input.GetKey(KeyCode.DownArrow) && !states.isPlayerOne)) // To crouch
+            {
+                animator.SetBool("Crouching", true);
+                movementcolliders[0].SetActive(false);
+                if ((Input.GetKey(KeyCode.F) && states.isPlayerOne) || (Input.GetKey(KeyCode.J) && !states.isPlayerOne)) // crouch punch
+                {
+                    animator.SetBool("LPunch", true);
+                // Debug.Log("You crouch punched");
+                    states.SelectCombatCollider(1, 10);
+                }
+                if ((Input.GetKey(KeyCode.G) && states.isPlayerOne) || (Input.GetKey(KeyCode.K) && !states.isPlayerOne)) // crouch kick
+                {
+                    animator.SetBool("LKick", true);
+                    //Debug.Log("You crouch kick");
+                    states.SelectCombatCollider(1, 10);
+                }
+            }
+            else
+            {
+                animator.SetBool("Crouching", false);
+                movementcolliders[0].SetActive(true);
+            }
+            if (numberAttack == 1) //  For the AI to punch
+            {
+                StartCoroutine(waitAfterAttack());
+                animator.SetBool("HPunch", true);
+                //Debug.Log("You Punched");
+                states.SelectCombatCollider(0, 10);
+                
+            }
+            if (numberAttack == 2) // kick
+            {
+                StartCoroutine(waitAfterAttack());
+                animator.SetBool("LPunch", true);
+            // Debug.Log("You Kicked");
+                states.SelectCombatCollider(0, 10);
+            }
+
+            if ((Input.GetKey(KeyCode.F) && states.isPlayerOne && !states.onGround) || (Input.GetKey(KeyCode.J) && !states.isPlayerOne && !states.onGround)) // jump punch
             {
                 animator.SetBool("LPunch", true);
-               // Debug.Log("You crouch punched");
+            // Debug.Log("You jump punched");
                 states.SelectCombatCollider(1, 10);
             }
-            if ((Input.GetKey(KeyCode.G) && states.isPlayerOne) || (Input.GetKey(KeyCode.K) && !states.isPlayerOne)) // crouch kick
+            if ((Input.GetKey(KeyCode.G) && states.isPlayerOne && !states.onGround) || (Input.GetKey(KeyCode.K) && !states.isPlayerOne && !states.onGround)) // jump kick
             {
-                animator.SetBool("LKick", true);
-                //Debug.Log("You crouch kick");
+                animator.SetBool("HKick", true);
+            // Debug.Log("You jump kick");
                 states.SelectCombatCollider(1, 10);
             }
-        }
-        else
-        {
-            animator.SetBool("Crouching", false);
-            movementcolliders[0].SetActive(true);
-        }
-        if (numberAttack == 1) //  For the AI to punch
-        {
-            StartCoroutine(waitAfterAttack());
-            animator.SetBool("HPunch", true);
-            //Debug.Log("You Punched");
-            states.SelectCombatCollider(0, 10);
-            
-        }
-        if (numberAttack == 2) // kick
-        {
-            StartCoroutine(waitAfterAttack());
-            animator.SetBool("LPunch", true);
-           // Debug.Log("You Kicked");
-            states.SelectCombatCollider(0, 10);
-        }
-
-        if ((Input.GetKey(KeyCode.F) && states.isPlayerOne && !states.onGround) || (Input.GetKey(KeyCode.J) && !states.isPlayerOne && !states.onGround)) // jump punch
-        {
-            animator.SetBool("LPunch", true);
-           // Debug.Log("You jump punched");
-            states.SelectCombatCollider(1, 10);
-        }
-        if ((Input.GetKey(KeyCode.G) && states.isPlayerOne && !states.onGround) || (Input.GetKey(KeyCode.K) && !states.isPlayerOne && !states.onGround)) // jump kick
-        {
-            animator.SetBool("HKick", true);
-           // Debug.Log("You jump kick");
-            states.SelectCombatCollider(1, 10);
         }
         states.CloseColliders(states.leftDamageColliders);
         states.CloseColliders(states.rightDamageColliders);
@@ -100,11 +106,18 @@ public class CombatAI : MonoBehaviour
 
     public void randomATT()
     {
-       //starting from -1 to add some more space inbetween attacks
-        numberAttack = Random.Range(-1,3); // number 1 - 2 just punch and kick for now mayber more later
+       
+        numberAttack = Random.Range(1,3); // number 1 - 2 just punch and kick for now mayber more later
+        StartCoroutine(SetAttacking());
         
         
 
+    }
+
+    IEnumerator SetAttacking()
+    {
+        yield return new WaitForSeconds(atackRate);
+        attacking = true;
     }
 
 
