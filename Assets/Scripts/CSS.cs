8b0950b0074e88068b2e4886dd90cf42b8ebacad
JunLin2001 <101033015+JunLin2001@users.noreply.github.com>
2023-04-17 03:32:32 -0400

    2character selection

--- Assets/Scripts/CSS.cs
+++ Assets/Scripts/CSS.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class CSS : MonoBehaviour
 {
     public GameObject optionPrefab;
-
     public Transform prevCharacter;
     public Transform selectedCharacter;
-
+    public Text playerIndicator;
+    private bool P1isLocked = false;
+    private bool P2isLocked = false;
+    private int currentPlayer = 1;
+    [SerializeField] private Image player1Image;
+    [SerializeField] private Image player2Image;
     private void Start()
     {
+        player1Image.gameObject.SetActive(false);
+        player2Image.gameObject.SetActive(false);
         foreach (Character c in GameManager.instance.characters)
         {
             GameObject option = Instantiate(optionPrefab, transform);
@@ -19,13 +25,41 @@ public class CSS : MonoBehaviour
 
             button.onClick.AddListener(() =>
             {
-                GameManager.instance.SetCharacter(c);
-                if (selectedCharacter != null)
+                if (currentPlayer == 1 && !P1isLocked)
                 {
-                    prevCharacter = selectedCharacter;
-                }
+                    GameManager.instance.SetCharacterPlayer1(c);
+                    player1Image.gameObject.SetActive(true);
+                    player1Image.sprite = c.icon;
+                    if (selectedCharacter != null)
+                    {
+                        prevCharacter = selectedCharacter;
+                    }
 
-                selectedCharacter = option.transform;
+                    selectedCharacter = option.transform;
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        P1isLocked = true;
+                        GameManager.instance.LockCharacterPlayer1();
+                        currentPlayer = 2;
+                        playerIndicator.text = "Player 2, choose your character.";
+                    }
+                }
+                else if (currentPlayer == 2 && !P2isLocked)
+                {
+                    GameManager.instance.SetCharacterPlayer2(c);
+                    player2Image.gameObject.SetActive(true);
+                    player2Image.sprite = c.icon;
+                    if (selectedCharacter != null)
+                    {
+                        prevCharacter = selectedCharacter;
+                    }
+                    selectedCharacter = option.transform;
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        P2isLocked = true;
+                        GameManager.instance.LockCharacterPlayer2();
+                    }
+                }
             });
             Text text = option.GetComponentInChildren<Text>();
             text.text = c.name;
@@ -37,14 +71,14 @@ public class CSS : MonoBehaviour
 
     private void Update()
     {
-        if (selectedCharacter != null)
+        if (selectedCharacter != null && !(P1isLocked && P2isLocked))
         {
             selectedCharacter.localScale = Vector3.Lerp(selectedCharacter.localScale, new Vector3(1.2f, 1.2f, 1.2f), Time.deltaTime * 10);
         }
-
         if (prevCharacter != null)
         {
             prevCharacter.localScale = Vector3.Lerp(prevCharacter.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * 10);
         }
     }
 }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     public static GameManager instance;
-
     public Character[] characters;
 
-    public Character currentCharacter;
+    public Character currentCharacterPlayer1;
+    public Character currentCharacterPlayer2;
 
+    private bool isPlayer1Locked = false;
+    private bool isPlayer2Locked = false;
 
     private void Awake()
     {
@@ -21,12 +22,34 @@ public class GameManager : MonoBehaviour
     {
         if (characters.Length > 0)
         {
-            currentCharacter = characters[0];
+            currentCharacterPlayer1 = characters[0];
+            currentCharacterPlayer2 = characters[0];
         }
     }
 
-    public void SetCharacter(Character character)
+    public void SetCharacterPlayer1(Character character)
     {
-        currentCharacter = character;
+        if (!isPlayer1Locked)
+        {
+            currentCharacterPlayer1 = character;       
+        }
+    }
+
+    public void SetCharacterPlayer2(Character character)
+    {
+        if (!isPlayer2Locked)
+        {
+            currentCharacterPlayer2 = character;
+        }
+    }
+
+    public void LockCharacterPlayer1()
+    {
+        isPlayer1Locked = true;
+    }
+
+    public void LockCharacterPlayer2()
+    {
+        isPlayer2Locked = true;
     }
 }
