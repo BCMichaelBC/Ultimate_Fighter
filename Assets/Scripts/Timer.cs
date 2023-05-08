26e8624b7eefa4dec892b9c3d641e2db7c2130f0
Winson Huang <winsonhuang2000@gmail.com>
2023-05-07 21:36:27 -0400

    pause panel to menu fix

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -39,69 +40,69 @@ public class Timer : MonoBehaviour
         }
         TimerText.text = Mathf.Round(LevelTime).ToString();
 
-         if (Input.GetKeyDown(KeyCode.P))
-         {
-             if (GamePaused)
-             {
-                 ResumeGame();
-             }
-             else
-             {
-                 PauseGame();
-             }
-         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
 
-         else if(LevelTime <= 0)
-         {
-            if(playerOne.Hp > playerTwo.Hp)
+        else if (LevelTime <= 0)
+        {
+            if (playerOne.Hp > playerTwo.Hp)
             {
                 print("Player One Wins");
 
                 levelManager.playerOneWins++;
-                if(levelManager.roundNumber == 1)
+                if (levelManager.roundNumber == 1)
                 {
                     levelManager.playerOneWinRoundOne.SetActive(true);
                 }
-                else if(levelManager.roundNumber >= 2)
+                else if (levelManager.roundNumber >= 2)
                 {
                     levelManager.playerOneWinRoundTwo.SetActive(true);
                 }
                 Reset();
             }
-            else if(playerTwo.Hp > playerOne.Hp)
+            else if (playerTwo.Hp > playerOne.Hp)
             {
                 print("Player Two Wins");
                 levelManager.playerTwoWins++;
-                if(levelManager.roundNumber == 1)
+                if (levelManager.roundNumber == 1)
                 {
                     levelManager.playerTwoWinRoundOne.SetActive(true);
                 }
-                else if(levelManager.roundNumber >= 2)
+                else if (levelManager.roundNumber >= 2)
                 {
                     levelManager.playerTwoWinRoundTwo.SetActive(true);
                 }
                 Reset();
             }
-            else if(playerTwo.Hp == playerOne.Hp)
+            else if (playerTwo.Hp == playerOne.Hp)
             {
                 print("Draw");
             }
-         }
+        }
     }
 
     void Reset()
     {
         playerOne.CheckWinner();
-            playerOne.Hp = 100;
-            playerTwo.Hp = 100;
-            playerOneState.health = 100;
-            playerTwoState.health = 100;
-            playerOneHealth.UpdateHpBar();
-            playerTwoHealth.UpdateHpBar();
-            LevelTime = 90;
-            TimerText.text = Mathf.Round(LevelTime).ToString();
-            playerOne.transform.position = new Vector3(-6f, -1f, 0f);
-            playerTwo.transform.position = new Vector3(6f, -1f, 0f);
+        playerOne.Hp = 100;
+        playerTwo.Hp = 100;
+        playerOneState.health = 100;
+        playerTwoState.health = 100;
+        playerOneHealth.UpdateHpBar();
+        playerTwoHealth.UpdateHpBar();
+        LevelTime = 90;
+        TimerText.text = Mathf.Round(LevelTime).ToString();
+        playerOne.transform.position = new Vector3(-6f, -1f, 0f);
+        playerTwo.transform.position = new Vector3(6f, -1f, 0f);
     }
 
     void PauseGame()
@@ -116,6 +117,15 @@ public class Timer : MonoBehaviour
         GamePaused = false;
         PausePanel.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("StartScreen");
+        PausePanel.SetActive(false);
+        GamePaused = false;
+        Time.timeScale = 1f;
+
     }
     IEnumerator roundcountdown()
     {
